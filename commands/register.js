9a1fa91b39ef02701a0f4d950cee75b6a627735a
thedeliriousdev <74074440+jtlarrabee@users.noreply.github.com>
2023-02-09 16:05:18 -0500

    Figuring out writing to DynamoDB

--- commands/register.js
+++ commands/register.js
@@ -12,19 +12,43 @@ module.exports = {
 		.setDescription('Register with the League as a new Pokémon trainer!'),
     async execute(interaction) {
         // Query the table to see if  the user has already registered.
-        const params = {
+        const regCheckParams = {
             KeyConditionExpression: "entity_id = :t",
             ExpressionAttributeValues: {
-                ":t": { S: `${interaction.user.id}`}
+                ":t": { S: interaction.user.id}
             },
             TableName: "pkmn-delirious-table"
         };
 
-        const data = await ddbClient.send(new ddb.QueryCommand(params));
+        const regCheckData = await ddbClient.send(new ddb.QueryCommand(regCheckParams));
+        console.log(regCheckData);
 
         // Register the user if they have not already registered.
-        if (data.Count == 0) {
-            // Allow the user to pick a starter Pokémon.
+        if (regCheckData.Count == 1) {
+            // Create the new trainer object.
+            const newtrainer = {
+                "userID": interaction.user.id,
+                "pokemon": {
+                    "main": 'None',
+                    "team": [],
+                    "pc": []
+                },
+                "inventory": interaction.user.id + '-inv'
+            };
+
+            console.log(newtrainer);
+
+            // Push the trainer object to DynamoDB.
+            const regTrainerParams = {
+                TableName: "pkmn-delirious-table",
+                Item: {
+                    entity_id: interaction.user.id
+                }
+            };
+
+            await ddbClient.send(new ddb.PutItemCommand(regTrainerParams));
+
+            /* Allow the user to pick a starter Pokémon.
             const row = new ActionRowBuilder()
             .addComponents(
                 new SelectMenuBuilder()
@@ -49,7 +73,7 @@ module.exports = {
                     )
             );
 
-            await interaction.reply({ components: [row] });
+            await interaction.reply({ components: [row] }); */
         };
     }
 };
